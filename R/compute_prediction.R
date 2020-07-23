1b28e8c48514f12d00e9aa4018d17d68909baa98
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2020-07-23 18:09:25 +0200

    add choose_delta in compute_prediction

--- R/compute_prediction.R
+++ R/compute_prediction.R
@@ -4,6 +4,7 @@
 #' @param estimates a list, default output of the mapbay_estimation
 #' @param model a mapbay_model list, with the characteristics of the model, including a compiled mrgsolve_model
 #' @param time_target a numeric, time needed for reestimation of concentration
+#' @param choose_delta a numeric, delta for simulation process (default = .1)
 #'
 #' @return a dataframe, a mrgsolve 'df' output
 #' @export
@@ -11,7 +12,7 @@
 #' @importFrom dplyr as_tibble
 #' @importFrom magrittr %>%
 #'
-compute_prediction <- function(data, estimates, model, time_target){
+compute_prediction <- function(data, estimates, model, time_target, choose_delta = .1){
 
   t_end = max(max(data[["time"]]), time_target) # if sampling time > to theoretical trough time
 
@@ -20,7 +21,7 @@ compute_prediction <- function(data, estimates, model, time_target){
     zero_re %>%
     data_set(data) %>%
     obsaug() %>%
-    mrgsim_df(end = t_end, delta = .1, carry_out = c("evid", "cmt", "mdv")) %>%
+    mrgsim_df(end = t_end, delta = choose_delta, carry_out = c("evid", "cmt", "mdv")) %>%
     as_tibble()
 
 }
