cf9ae2e9f6541257261c2d7e0e56f000f80a7160
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2020-10-19 10:51:18 +0200

    add function to plot posterior distribution

--- R/zzz_utils.R
+++ R/zzz_utils.R
@@ -1,12 +1,13 @@
 #' @importFrom dplyr all_of any_of arrange as_tibble distinct filter mutate pull rename_with right_join select slice starts_with vars
-#' @importFrom ggplot2 ggplot aes facet_wrap geom_line geom_point geom_hline labs theme_light scale_x_continuous scale_color_manual facet_grid %+replace% theme label_both
+#' @importFrom ggplot2 ggplot aes facet_wrap geom_line geom_point geom_hline geom_vline labs theme_light theme_bw scale_x_continuous scale_color_manual stat_function facet_grid %+replace% theme label_both
+#' @importFrom ggpubr ggarrange
 #' @importFrom magrittr %>%
 #' @importFrom minqa newuoa
 #' @importFrom mrgsolve mread mrgsim mrgsim_df param zero_re data_set obsaug omat smat as.list
-#' @importFrom purrr map
+#' @importFrom purrr map pmap
 #' @importFrom rlang .data set_names splice
-#' @importFrom stats runif optim
+#' @importFrom stats runif optim dnorm
 #' @importFrom stringr str_subset str_remove str_squish str_split str_detect str_which str_to_title str_replace str_c
-#' @importFrom tibble enframe
+#' @importFrom tibble tibble enframe
 #' @importFrom tidyr replace_na pivot_longer
 NULL
