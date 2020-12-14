b616d3a7b25e5b671c7b3c231bf07cc62207c6cc
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2020-12-14 18:34:15 +0100

    s3 method as.data.frame

--- R/s3_methods.R
+++ R/s3_methods.R
@@ -1,10 +1,39 @@
 #' Print a mbrests object
 #'
 #' @param x A \code{mbrests} object.
-#' @param ... further arguments passed to or from other methods
+#' @param ... additional arguments
 #'
 #' @method print mbrests
 #' @export
 print.mbrests <- function(x, ...){
-  print(x$mapbay_tab, ...)
+  NAME <- mbr_model_name(x$model)
+  nID <- length(x$arg.ofv)
+  nOBS <- x$arg.ofv %>% map("DVobs") %>% unname() %>%  simplify() %>% length()
+  nETA <- length(x$arg.optim$par)
+  ETA <- x$final_eta %>%
+    bind_rows(.id = "ID") %>%
+    as.data.frame() %>%
+    utils::head()
+  TAB <- utils::head(as.data.frame(x$mapbay_tab))
+
+  cat(NAME, "\n")
+  cat("ID :", nID, " individual(s).\n")
+  cat("OBS:", nOBS, " observation(s).\n")
+  cat("ETA:", nETA, " parameter(s) to estimate.\n\n")
+  cat("Estimates: \n")
+  print(ETA)
+  cat("\nOutput (", nrow(x$mapbay_tab) , " lines): \n", sep = "")
+  print(TAB)
+}
+
+
+#' Return the mapbay_tab as a data.frame
+#'
+#' @param x A \code{mbrests} object.
+#' @param row.names,optional,... passed to as.data.frame
+#'
+#' @method as.data.frame mbrests
+#' @export
+as.data.frame.mbrests <- function(x, row.names = NULL, optional = FALSE, ...){
+  as.data.frame(x$mapbay_tab, ...)
 }
