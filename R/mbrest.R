346dbdcaee447f710b41fa0e03911ad04129e69a
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2021-02-09 19:02:54 +0100

    checking mapbayr specifications in model

--- R/mbrest.R
+++ R/mbrest.R
@@ -21,9 +21,14 @@ mbrest <- function(x,
                    control = list(),
                    force_initial_eta = NULL,
                    quantile_bound = 0.001,
-                   reset = T){
-  ok <- check_mapbayr_model(x)
-  if(!isTRUE(ok)) stop(c("\n", paste(ok, collapse = "\n")), call. = F)
+                   reset = T,
+                   check = T){
+  if(check){
+    ok <- check_mapbayr_model(x)
+    if(!isTRUE(ok)){
+      if(any(ok$stop)) stop(paste(ok[ok$stop==T,]$descr, collapse = "\n"), call. = F)
+    }
+  }
 
   arg.optim <- preprocess.optim(method = method, model = x, control = control, force_initial_eta = force_initial_eta, quantile_bound = quantile_bound)
 
@@ -62,21 +67,44 @@ mbrest <- function(x,
 #' library(mrgsolve)
 #' check_mapbayr_model(house())
 check_mapbayr_model <- function(x){
- # browser()
+  # browser()
   if(!is.mrgmod(x)){
-    stop("the first argument to mbrest must be a model object", call. = F)
+    stop("the first argument must be a model object", call. = F)
   }else{
-    check <- character()
-    if(!"DV" %in% x@capL) check <- c(check, "DV must be present in $CAPTURE")
-    if(is.null(obs_cmt(x))) check <- c(check, "No [OBS] compartment(s) defined in $CMT")
-    if(is.null(adm_cmt(x))) check <- c(check, "No [ADM] compartment(s) defined in $CMT")
+    check <- tibble(stop = logical(0), descr = character(0))
 
-    #@param has eta  ?
-    #@param = as many as
+    # $PARAM
+    neta <- length(eta_names(x))
+    if(neta == 0) {
+      check <- bind_rows(check, list(stop = TRUE, descr = "$PARAM: No ETA (ETA1, ETA2...) defined."))
+    } else {
+      if(any(eta_names(x) != paste0("ETA", seq.int(length.out = neta)))) check <-  bind_rows(check, list(stop = TRUE, descr = paste0("$PARAM: ", neta, " ETA found, but not sequentially named ETA1.")))
+      if(!all(x[eta_names(x)]==0)) check <- bind_rows(check, list(stop = TRUE, descr = "$PARAM: Initial value is not 0 for all ETA."))
+      if(any(is.na(eta_descr(x)))) check <- bind_rows(check, list(stop = FALSE, descr = "$PARAM: Description missing for at least one ETA (optionnal)."))
+    }
 
+    # $CMT
+    if(is.null(adm_cmt(x))) check <- bind_rows(check, list(stop = FALSE, descr = "$CMT: No [ADM] compartment(s) defined (optionnal)."))
+    if(is.null(obs_cmt(x))) check <- bind_rows(check, list(stop = FALSE, descr = "$CMT: No [OBS] compartment(s) defined (optionnal)."))
 
+    # $OMEGA as much as ETA ?
+    nomega <- length(diag(omat(x, make = T)))
+    if(nomega != neta) check <- bind_rows(check, list(stop = TRUE, descr = "$OMEGA: Length of omega matrix diagonal not equal to the number of ETA defined in $PARAM."))
+
+    # $SIGMA
+    nsig <- length(diag(smat(x, make = T)))
+    if(is.null(obs_cmt(x))){
+      if(nsig != 2) check <- bind_rows(check, list(stop = TRUE,  descr = "$SIGMA: Define only one pair of sigma values (prop + add errors) in $SIGMA if you do not use [OBS] in $CMT. (One observation compartment will be defined from MDV=0 lines in individual data"))
+    } else {
+      ncmt <- length(obs_cmt(x))
+      if(ncmt != nsig/2) check <- bind_rows(check, list(stop = TRUE, descr = "$SIGMA: Define one pair of sigma values (prop + add errors) per [OBS] compartment(s) defined in $CMT."))
+    }
+
+    # $CAPTURE
+    if(!"DV" %in% x@capL) check <- bind_rows(check, list(stop = TRUE,  descr = "$CAPTURE: DV must be captured."))
+    if(any(!(c("PAR", "MET") %in% x@capL)) & nsig > 2) check <- bind_rows(check, list(stop = TRUE,  descr = "$CAPTURE PAR and MET must be captured if multiple types of DV are fitted (more than one pair of sigma provided in $SIGMA)"))
 
   }
-  if(length(check)==0) check <- TRUE
+  if(nrow(check)==0) check <- TRUE
   return(check)
 }
--- tests/testthat/test-read_mrgsolve_model.R
+++ tests/testthat/test-read_mrgsolve_model.R
@@ -1,16 +1,7 @@
-#test_that("read model specifications", {
-#  mod1 <- mread("ex_mbr1", mbrlib())
-#  expect_equal(mbr_drug(mod1), "Examplinib1")
-#  expect_equal(mbr_model_ref(mod1), "XXX et al, J Pharmacokinet, 2020")
-#  expect_equal(mbr_model_file(mod1), "ex_mbr1")
-#  expect_equal(mbr_model_name(mod1), "Ex Mbr1")
-#})
-
 test_that("read cmts", {
   mod1 <- mread("ex_mbr1", mbrlib())
   mod2 <- mread("ex_mbr2", mbrlib())
   mod3 <- mread("ex_mbr3", mbrlib())
-
   expect_equal(adm_cmt(mod1), c(1,2))
   expect_equal(adm_cmt(mod2), 1)
   expect_equal(adm_cmt(mod3), 1)
@@ -20,5 +11,151 @@ test_that("read cmts", {
   expect_equal(obs_cmt(mod3), 1)
 })
 
+test_that("Detection of cmt to fit in data is correct", {
 
+  # === Reminder : how it should work: ===
+  # - If [OBS] is provided, it defines fit_cmt.
+  ##1) Thus, the length of sigma matrix diagonal must be 2 x number of compartments
+  ##2) Thus, obs in data must be defined in model
+
+  # - If [OBS] is not provided, mapbayr will search for one observation compartment on MDV == 0 lines in EACH individual
+  ##3) If more than one compartment with observation in the dataset (in one individual) : stop, user should use [OBS] if multiple obs compartments are used
+  ##4) Compartment must exist in the model
+  ##5) Sigma must be equal to 2
+
+
+  basecode <- "
+$PARAM @annotated
+TVCL:  0.5 : Clearance
+TVV1: 20.0 : Central volume
+V2  : 10.0 : Peripheral volume of distribution
+Q   :  3.0 : Intercompartmental clearance
+ETA1: 0 : Clearance (L/h)
+ETA2: 0 : Central volume (L)
+$OMEGA 0.3 0.2
+$TABLE
+double DV = (CENT/V1) *(1 + EPS(1)) + EPS(2);
+$MAIN
+double CL = TVCL * exp(ETA1 + ETA(1)) ;
+double V1 = TVV1 * exp(ETA2 + ETA(2)) ;
+double K12 = Q / V1  ;
+double K21 = Q / V2  ;
+double K10 = CL / V1 ;
+$ODE
+dxdt_CENT   =  K21 * PERIPH - (K10 + K12) * CENT ;
+dxdt_PERIPH =  K12 * CENT - K21 * PERIPH ;
+$CAPTURE DV
+  "
+
+  # Let's go. Here [OBS] is defined in $CMT
+  sigma0 <- "$SIGMA
+0.06 // proportional
+0.1 // additive
+  "
+
+  cmt0 <- "$CMT @annotated
+CENT  : Central compartment (mg/L)[ADM, OBS]
+PERIPH: Peripheral compartment ()
+  "
+
+  mod1 <- mcode("mod1", code = paste0(basecode, sigma0, cmt0))
+  data1 <- mod1 %>%
+    adm_lines(amt = 100) %>%
+    obs_lines(time = 24, DV = 6) %>%
+    obs_lines(time = 36, DV = 3) %>%
+    see_data()
+
+  #This works
+  expect_s3_class(mbrest(mod1, data1, verbose = F), "mbrests")
+
+  #1 Modified code to test point 1 = the length of sigma matrix diagonal must be 2 x number of compartments
+  sigma1 <- "$SIGMA
+0.06 // proportional
+0.1 // additive
+0.2
+  "
+  mcode("mod1", code = paste0(basecode, sigma1, cmt0)) %>%
+    mbrest(data = data1) %>%
+    expect_error(".*SIGMA: Define one pair of sigma values.*")
+
+  #2 Modified data to test point 2 = obs in data must be defined in model
+  data2 <- mutate(data1, cmt = c(1, 1, 99))
+  expect_error(mbrest(x = mod1, data2), "ID =1; CMT =99\n One or more compartment with observation .* in data don't match those defined with")
+
+  #Other type of model. Here [OBS] is NOT defined in $CMT
+  cmt3 <- "$CMT CENT PERIPH"
+  mod3 <- mcode("mod3", code = paste0(basecode, sigma0, cmt3))
+
+  check3 <- check_mapbayr_model(mod3)
+  expect_true("$CMT: No [ADM] compartment(s) defined (optionnal)." %in% check3$descr)
+  expect_true("$CMT: No [OBS] compartment(s) defined (optionnal)." %in% check3$descr)
+  expect_equal(fit_cmt(mod3, data1), 1)
+
+  #This works
+  expect_s3_class(mbrest(mod3, data1, verbose = F), "mbrests")
+
+  ##3) Only one obs compartment in data
+  data3 <- mutate(data1, cmt = c(1,2,1))
+  mbrest(mod3, data3) %>% expect_error("More than one 'observation compartment' to detect from data. Consider editing model code")
+
+  ##4) Compartment must exist in the model
+  data4 <- mutate(data1, cmt = c(1,99,99))
+  mbrest(mod3, data4) %>% expect_error("Compartment number with observation in dataset does not exist in model.")
+
+  ##5) Sigma must be equal to 2
+
+  mcode("mod5", code = paste0(basecode, sigma1, cmt3)) %>%
+    mbrest(data = data1) %>%
+    expect_error(".*Define only one pair of sigma values .* if you do not use .*One observation compartment will be defined from MDV=0 lines in individual data")
+
+})
+
+
+test_that("wrong code is correctly checked", {
+  code1 <- "
+$PARAM @annotated
+TVCL:  0.5 : Clearance
+TVV1: 20.0 : Central volume
+V2  : 10.0 : Peripheral volume of distribution
+Q   :  3.0 : Intercompartmental clearance
+
+ETA1: 0 : Clearance (L/h)
+ETA_2: 0 : Central volume (L) //                     error here
+ETA3: .1://                                           error here
+
+$OMEGA 0.3 0.2 0.1
+$SIGMA
+0.06 // proportional
+0.1 // additive
+
+$CMT CENT PERIPH
+
+$TABLE
+double DV = (CENT/V2) *(1 + EPS(1)) + EPS(2);
+
+$MAIN
+double CL = TVCL * exp(ETA1 + ETA(1)) ;
+double V1 = TVV1 * exp(ETA(2)) ; //                       error
+double K12 = (Q / V1) * exp(ETA3 + ETA(3)) ;
+double K21 = Q / V2  ;
+double K10 = CL / V1 ;
+
+$ODE
+dxdt_CENT   =  K21 * PERIPH - (K10 + K12) * CENT ;
+dxdt_PERIPH =  K12 * CENT - K21 * PERIPH ;
+
+$CAPTURE DV
+"
+
+  check1 <- check_mapbayr_model(mcode("test1", code = code1))
+
+  expect_true("$PARAM: 2 ETA found, but not sequentially named ETA1." %in% check1$descr)
+  expect_true("$PARAM: Initial value is not 0 for all ETA." %in% check1$descr)
+  expect_true("$PARAM: Description missing for at least one ETA (optionnal)." %in% check1$descr)
+  expect_true("$OMEGA: Length of omega matrix diagonal not equal to the number of ETA defined in $PARAM." %in% check1$descr)
+  expect_true("$CMT: No [ADM] compartment(s) defined (optionnal)." %in% check1$descr)
+  expect_true("$CMT: No [OBS] compartment(s) defined (optionnal)." %in% check1$descr)
+  expect_false("$SIGMA: Define only one pair of sigma values (prop + add errors) in $SIGMA if you do not use [OBS] in $CMT. (One observation compartment will be defined from MDV=0 lines in individual data" %in% check1$descr)
+
+})
 
