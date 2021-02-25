5b016bb823c605eb7218458c11c1073cbab05504
Félicien Le Louedec <felicienlelouedec@MacBook-Pro-de-Felicien.local>
2021-02-25 18:33:53 +0100

    eta within bounds when reset

--- R/mbrest_optimization.R
+++ R/mbrest_optimization.R
@@ -6,8 +6,7 @@ do_optimization <- function(arg.ofv, arg.optim, verbose, reset){
   opt <- do.call(quietly(optimx), c(arg.optim, arg.ofv))$result
 
   while(RUN <= 50 && reset && (check_etavalue(opt, arg.ofv, arg.optim)|check_convcode(OPT = opt))){
-    set.seed(123+RUN)
-    arg.optim$par <- new_ini2(arg.ofv, arg.optim)
+    arg.optim$par <- new_ini2(arg.ofv, arg.optim, run = RUN)
 
     warning("\nError in optimization. Reset with initial values: ", paste(arg.optim$par, collapse = ' '), call. = F, immediate. = T)
     opt <- do.call(quietly(optimx), c(arg.optim, arg.ofv))$result
@@ -29,34 +28,14 @@ initial_ofv <- function(arg.ofv, arg.optim){
   do.call(compute_ofv, c(list(eta = arg.optim$par), arg.ofv))
 }
 
-
-new_ini <- function(arg.ofv, arg.optim){
-  map2(arg.optim$lower, arg.optim$upper, runif, n = 3) %>%
-    transpose() %>%
-    map(diag) %>%
-    map_dfr(as_tibble) %>%
-    rename_with(str_replace, everything(), "V","ETA") %>%
-    map(unlist) %>%
-    transpose() %>%
-    map(unlist) %>%
-    map_dfr(function(x){
-      ofv <- do.call(compute_ofv, c(list(eta = x), arg.ofv))
-      c(unlist(x), OFV = ofv)
-    }) %>%
-    slice_min(.data$OFV, with_ties = FALSE) %>%
-    select(-.data$OFV) %>%
-    unlist() %>%
-    round(6)
-}
-
-new_ini2 <- function(arg.ofv, arg.optim){
- # browser()
-  mvgauss(solve(arg.ofv$omega.inv), n = 1+n_eta(arg.ofv$mrgsolve_model)^2) %>%
+new_ini2 <- function(arg.ofv, arg.optim, run){
+  mvgauss(solve(arg.ofv$omega.inv), n = 1+n_eta(arg.ofv$mrgsolve_model)^2, seed = 1+run) %>%
     as.data.frame() %>%
     rename_with(str_replace, everything(), "V","ETA") %>%
     map(unlist) %>%
     transpose() %>%
     map(unlist) %>%
+    map(~ ifelse(abs(.x) > arg.optim$upper, 0, .x)) %>%
     map_dfr(function(x){
       ofv <- do.call(compute_ofv, c(list(eta = x), arg.ofv))
       c(unlist(x), OFV = ofv)
---
+++ tests/testthat/test-newini2.R
@@ -0,0 +1,20 @@
+test_that("new_ini2 works", {
+
+  mod <- mread("ex_mbr3", mbrlib())
+
+  data <- mod %>%
+    adm_lines(amt = 100) %>%
+    obs_lines(time = 12, DV = 3) %>%
+    see_data()
+
+  argofv <- preprocess.ofv(x = mod, data = data)
+  argopt <- preprocess.optim(x = mod, method = "L-BFGS-B", control = list(), force_initial_eta = NULL,
+                             quantile_bound = .4) # !!
+
+  ini <- new_ini2(arg.ofv = argofv, arg.optim = argopt, run = 1)
+
+  expect_type(ini, "double")
+  expect_true(all(ini > argopt$lower))
+  expect_true(all(ini < argopt$upper))
+
+})
