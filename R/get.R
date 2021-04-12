2a226052ec41fd8e0b947c2959d5bf78a8f164e3
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2021-04-12 15:43:31 +0200

    add get_eta

--- R/get.R
+++ R/get.R
@@ -36,3 +36,47 @@ get_data.mapbayests <- function(x, ...){
   as_tibble(x$data)
 }
 
+
+
+
+
+#' @rdname get_x
+#' @export
+get_eta <- function(x, ...) UseMethod("get_eta")
+
+
+#' Return eta from a mapbayests
+#'
+#' @param x mapbayests object
+#' @param ... not used
+#' @param output either a list ("list"), a data.frame ("df") or a vector of numeric ("num"). Default to "num" if only one ID.
+#'
+#' @method get_eta mapbayests
+#' @return a tibble
+#' @export
+get_eta.mapbayests <- function(x, ..., output = NULL){
+
+  oneID <- (length(x$arg.ofv.id) == 1)
+
+  if(is.null(output)){
+    if(oneID){
+      .out <- "num"
+    } else {
+      .out <- "df"
+    }
+  } else {
+    okout <- c("num", "list", "df")
+    if(!output %in% okout) stop('Allowed output are: ', paste(okout, collapse = ", "), ".")
+    .out <- output[1]
+  }
+
+  if(!oneID & .out == "num") stop("Multiple ID, cannot coerce list to a vector of numeric.")
+
+  e <- switch (.out,
+    "num" = x$final_eta[[1]],
+    "list"= x$final_eta,
+    "df" = bind_rows(x$final_eta, .id = "ID")
+  )
+
+  return(e)
+}
---
+++ tests/testthat/test-get_eta.R
@@ -0,0 +1,79 @@
+mod <- mread('ex_mbr1', mbrlib())
+data1 <- mod %>%
+  adm_lines(amt = 10, addl = 2, ii = 12) %>%
+  obs_lines(DV = c(.1, .2), time = c(18, 40)) %>%
+  add_covariates(list(WT = 70)) %>%
+  get_data()
+
+data2 <- mod %>%
+  adm_lines(amt = 10, addl = 2, ii = 12) %>%
+  obs_lines(DV = c(.1, .2), time = c(19, 41)) %>%
+  add_covariates(list(WT = 70)) %>%
+  get_data() %>%
+  mutate(ID = 6)
+
+data12 <- bind_rows(data1, data2)
+
+est1 <- mapbayest(mod, data1, verbose = F)
+est12 <- mapbayest(mod, data12, verbose = F)
+
+get_eta(est1) %>% class()
+get_eta(est12) %>% class()
+
+
+test_that("default get_eta works", {
+  e1 <- get_eta(est1)
+  e12 <- get_eta(est12)
+
+  expect_length(e1, 3)
+  expect_type(e1, "double")
+  expect_named(e1, c("ETA1", "ETA2", "ETA3"))
+
+  expect_length(e12, 1+3)
+  expect_s3_class(e12, "data.frame")
+  expect_named(e12, c("ID", "ETA1", "ETA2", "ETA3"))
+
+})
+
+test_that("get_eta list works", {
+  e1 <- get_eta(est1, output = "list")
+  e12 <- get_eta(est12, output = "list")
+
+  expect_length(e1, 1)
+  expect_type(e1, "list")
+  expect_named(e1, c("1"))
+
+  expect_length(e12, 2)
+  expect_type(e12, "list")
+  expect_named(e12, c("1", "6"))
+})
+
+test_that("get_eta num works", {
+  e1 <- get_eta(est1, output = "num")
+
+  expect_length(e1, 3)
+  expect_type(e1, "double")
+  expect_named(e1, c("ETA1", "ETA2", "ETA3"))
+
+  expect_error(get_eta(est12, output = "num"), "Multiple ID, cannot coerce list to a vector of numeric.")
+})
+
+test_that("get_eta df works", {
+  e1 <- get_eta(est1, output = "df")
+  e12 <- get_eta(est12, output = "df")
+
+  expect_length(e1, 4)
+  expect_s3_class(e1, "data.frame")
+  expect_named(e1, c("ID", "ETA1", "ETA2", "ETA3"))
+  expect_equal(nrow(e1), 1)
+
+  expect_length(e12, 4)
+  expect_s3_class(e12, "data.frame")
+  expect_named(e12, c("ID", "ETA1", "ETA2", "ETA3"))
+  expect_equal(nrow(e12), 2)
+})
+
+test_that("get_eta stops if invalid type", {
+  expect_error(get_eta(est1, output = "AAA"), "Allowed output are: ")
+
+})
