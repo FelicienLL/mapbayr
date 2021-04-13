c7776591aea03903de9ed49340676987ed2a6a68
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2021-04-13 11:04:32 +0200

    add get_param

--- R/get.R
+++ R/get.R
@@ -36,3 +36,44 @@ get_data.mapbayests <- function(x, ...){
   as_tibble(x$data)
 }
 
+#' @rdname get_x
+#' @export
+get_param <- function(x, ...) UseMethod("get_param")
+
+#' Return a posteriori param from a mapbayests
+#'
+#' @param x mapbayests object
+#' @param ... passed along
+#'
+#' @method get_param mapbayests
+#' @return a tibble
+#' @export
+get_param.mapbayests <- function(x, ...){
+
+  selected <- unique(unlist(list(...)))
+
+  captured <- x$model@capL
+  ok_captured <- captured[!captured %in% c("DV", "PAR", "MET")]
+
+  if(length(selected) == 0){
+    ok_names <- ok_captured
+  } else {
+    ok_names <- selected[selected %in% ok_captured]
+  }
+
+  p <- x$mapbay_tab %>%
+    select(.data$ID, dplyr::any_of(ok_names)) %>%
+    group_by(.data$ID) %>%
+    dplyr::slice(1) %>%
+    dplyr::ungroup()
+
+  oneID <- (length(x$arg.ofv.id) == 1)
+
+  if(oneID){
+    p2 <- unlist(select(p, -.data$ID))
+  } else {
+    p2 <- p
+  }
+
+  return(p2)
+}
