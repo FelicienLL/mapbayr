823ad313ead395796ef1b210c9ba759b69c51f24
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2021-06-04 16:08:37 +0200

    Some slight documentation for unexported functions

--- R/mapbayest_optimization.R
+++ R/mapbayest_optimization.R
@@ -1,9 +1,17 @@
 
+#### do_optimization ####
+# This is the function that realize optimization from the argument arg.ofv, arg.optim etc...
 
 do_optimization <- function(arg.ofv, arg.optim, verbose, reset){
+
+  # First the optimization is done once.
+
   RUN <- 1
   if(verbose) cat(paste0("\nID ", unique(arg.ofv$data$ID), "..."))
   opt <- do.call(quietly(optimx), c(arg.optim, arg.ofv))$result
+
+
+  # Secondly, if conditions for a reset are met, a new optimization from initial eta values is done until reset is not needed.
 
   while(RUN <= 50 && reset == T && check_need_reset(OPT = opt, arg.ofv = arg.ofv, arg.optim = arg.optim)){
     arg.optim$par <- new_ini2(arg.ofv, arg.optim, run = RUN)
@@ -12,13 +20,19 @@ do_optimization <- function(arg.ofv, arg.optim, verbose, reset){
     opt <- do.call(quietly(optimx), c(arg.optim, arg.ofv))$result
 
     RUN <- RUN + 1
-
   }
+
   opt$run <- RUN
 
   if(verbose) cat(" done.\n")
   return(opt)
 }
+
+
+
+
+#### check results of optimization ####
+# 1 -  One function to test all the reset conditions
 
 check_need_reset <- function(OPT, arg.ofv, arg.optim){
   #If not all conditions TRUE = a reset is needed
@@ -29,6 +43,7 @@ check_need_reset <- function(OPT, arg.ofv, arg.optim){
     )
 }
 
+# 2 -  Unit functions that test a particular reset condition
 check_convcode <- function(OPT){
   #Success condition: `convcode` variable is 0 (meaning no error returned by optim).
   OPT$convcode == 0
@@ -48,24 +63,33 @@ check_absolute_eta <- function(OPT, arg.ofv){
   length(unique(abs(vec))) != 1
 }
 
-initial_ofv <- function(arg.ofv, arg.optim){
-  do.call(compute_ofv, c(list(eta = arg.optim$par), arg.ofv))
-}
+
+
+#### Reset initial value of ETA ####
+# Returns a vector of ETA to start the new estimation from:
 
 new_ini2 <- function(arg.ofv, arg.optim, run){
-  mvgauss(solve(arg.ofv$omega.inv), n = 1+n_eta(arg.ofv$mrgsolve_model)^2, seed = 1+run) %>%
+  mvgauss(solve(arg.ofv$omega.inv), n = 1+n_eta(arg.ofv$mrgsolve_model)^2, seed = 1+run) %>% #Sample 1+(Neta x Neta) vectors from prior MVN distribution
     as.data.frame() %>%
     rename_with(str_replace, everything(), "V","ETA") %>%
     map(unlist) %>%
     transpose() %>%
     map(unlist) %>%
-    map(~ ifelse(abs(.x) > arg.optim$upper, 0, .x)) %>%
-    map_dfr(function(x){
+    map(~ ifelse(abs(.x) > arg.optim$upper, 0, .x)) %>%   # If out of bounds value of eta, set it to zero.
+    map_dfr(function(x){                                  # Compute OFV for every vector value
       ofv <- do.call(compute_ofv, c(list(eta = x), arg.ofv))
       c(unlist(x), OFV = ofv)
     }) %>%
-    slice_min(.data$OFV, with_ties = FALSE) %>%
+    slice_min(.data$OFV, with_ties = FALSE) %>%           # Keep the lowest one
     select(-.data$OFV) %>%
     unlist() %>%
     round(6)
 }
+
+
+#### Helpers ####
+# Compute the initial value of OFV #maybe delete it ? only used in `check_final_ofv()` ?
+initial_ofv <- function(arg.ofv, arg.optim){
+  do.call(compute_ofv, c(list(eta = arg.optim$par), arg.ofv))
+}
+
