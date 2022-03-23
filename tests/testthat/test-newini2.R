db5872ef1b0d8a6343dd2548738506320801d801
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2022-03-23 10:42:19 +0100

    test new_ini3

--- tests/testthat/test-newini2.R
+++ tests/testthat/test-newini2.R
@@ -5,6 +5,7 @@ test_that("new_ini2 works", {
   data <- mod %>%
     adm_lines(amt = 100) %>%
     obs_lines(time = 12, DV = 3) %>%
+    obs_lines(time = 24, DV = 1) %>%
     get_data()
 
   argofv <- c(preprocess.ofv.fix(x = mod), preprocess.ofv.id(x = mod, iddata = data))
@@ -12,10 +13,15 @@ test_that("new_ini2 works", {
   argopt <- preprocess.optim(x = mod, method = "L-BFGS-B", control = list(), force_initial_eta = NULL,
                              quantile_bound = .4) # !!
 
-  ini <- new_ini2(arg.ofv = argofv, arg.optim = argopt, run = 1)
 
-  expect_type(ini, "double")
-  expect_true(all(ini > argopt$lower))
-  expect_true(all(ini < argopt$upper))
+  ini2 <- new_ini2(arg.ofv = argofv, arg.optim = argopt, run = 1)
+
+  ini3 <- new_ini3(arg.ofv = argofv, arg.optim = argopt, run = 1)
+
+  expect_equal(ini2, ini3)
+  expect_type(ini3, "double")
+  expect_true(all(ini3 > argopt$lower))
+  expect_true(all(ini3 < argopt$upper))
 
 })
+
