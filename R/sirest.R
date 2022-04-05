4e2af324975eab7f06cebfa2eecb794007edc6d5
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2022-04-05 17:44:45 +0200

    start post process and plot methods

--- R/sirest.R
+++ R/sirest.R
@@ -3,6 +3,7 @@ sirest <- function(x, data, nM = 1e5, nm = 1e4, .seed){
   # To do :
   # - one ID only
   # - out preprocess
+  # - seed
 
 
   argofv <- c(preprocess.ofv.fix(x = x), preprocess.ofv.id(x = x, iddata = data))
@@ -95,4 +96,107 @@ chunk <- function(x, n) {
 merge_data_etamatrix <- function(data, matrix){
   data$ID <- NULL
   merge(data, cbind(ID = seq_len(nrow(matrix)), matrix))
+}
+
+postprocess.sir <- function(x, data, sir.value){
+  col_PRED <- mrgsim_df(zero_re(x),
+                        data = data,
+                        end = -1) %>%
+    select(PRED = DV)
+
+  col_IPRED <- mrgsim_df(zero_re(x),
+                         data = merge_data_etamatrix(data, sir.value),
+                         end = -1) %>%
+    group_by(time) %>%
+    summarise(IPRED_LOW = quantile(DV, 0.025),
+              IPRED = quantile(DV, 0.50),
+              IPRED_UP = quantile(DV, 0.975)
+    ) %>%
+    select(IPRED_LOW, IPRED, IPRED_UP)
+
+  sir_tab <- bind_cols(data, col_PRED, col_IPRED)
+
+  print(sir_tab)
+
+  return(invisible(list(
+    model = x,
+    data = data,
+    final_eta = sir.value,
+    sir_tab = sir_tab
+  )))
+}
+
+plot_sir <- function(x){
+  .delta = 0.25
+
+  PREDs <- mrgsim_df(zero_re(x$model, "sigma"),
+                     data = merge_data_etamatrix(x$data, mrgsolve::mvgauss(omat(x$model, make = TRUE), n = nrow(x$final_eta))),
+                     obsaug = TRUE,
+                     delta = .delta,
+                     obsonly = TRUE,
+                     recsort = 3) %>%
+    group_by(.data$time) %>%
+    summarise(LOW = quantile(DV, 0.025),
+              VAL = quantile(DV, 0.50),
+              UP = quantile(DV, 0.975)
+    )
+
+  IPREDs <- mrgsim_df(zero_re(x$model),
+                      data = merge_data_etamatrix(x$data, x$final_eta),
+                      obsaug = TRUE,
+                      delta = .delta,
+                      obsonly = TRUE,
+                      recsort = 3) %>%
+    group_by(time) %>%
+    summarise(LOW = quantile(DV, 0.025),
+              VAL = quantile(DV, 0.50),
+              UP = quantile(DV, 0.975)
+    )
+
+  list(PRED = PREDs,
+       IPRED = IPREDs) %>%
+    bind_rows(.id = "PREDICTION") %>%
+    ggplot(aes(x = time)) +
+    geom_ribbon(aes(ymin = LOW, ymax = UP, fill = PREDICTION, col = PREDICTION), alpha = .3) +
+    geom_line(aes(y = VAL, col = PREDICTION)) +
+    geom_point(aes(y = DV), data = filter(x$data, evid == 0), size = 3) +
+    scale_color_manual(values= c(IPRED = "black", PRED = "deepskyblue1")) +
+    scale_linetype_manual(values= c(IPRED = 1, PRED = 2)) +
+    scale_fill_manual(values= c(IPRED = "black", PRED = "deepskyblue1"))
+
+}
+
+hist_sir <- function(x, post_as_density = FALSE){
+  minlow <- min(x)
+  maxup <- max(x)
+  xvalues <- seq(minlow - 0.01, maxup + 0.01, 0.05)
+
+  density_tab <- sapply(X = sqrt(c(0.3, 0.2, 0.1)),
+                        FUN = dnorm,
+                        x = xvalues, mean = 0) %>%
+    rename_as_eta() %>%
+    cbind(x = xvalues) %>%
+    as_tibble() %>%
+    pivot_longer(-x)
+
+  sir_tab <- as_tibble(sirmat1) %>%
+    pivot_longer(everything())
+
+  p1 <-  ggplot() +
+    facet_wrap("name") +
+    geom_area(aes(x = .data$x, y = .data$value), data = density_tab, fill = "skyblue", alpha = .3) +
+    geom_line(aes(x = .data$x, y = .data$value), data = density_tab) +
+    theme_bw() +
+    theme(strip.background = element_rect(fill = "white"))+
+    scale_y_continuous(name = NULL, labels = NULL)+
+    scale_x_continuous(name = NULL, n.breaks = 10)
+
+  if(post_as_density){
+    p1 <- p1 +
+      ggplot2::geom_density(aes(x = .data$value, y = .data$..density..), data = sir_tab, alpha = .8, col = "black", fill = 'grey')
+  } else {
+    p1 <- p1 +
+      geom_histogram(aes(x = .data$value, y = .data$..density..), data = sir_tab, alpha = .8, col = "black", fill = 'grey', bins = 30)
+  }
+  return(p1)
 }
