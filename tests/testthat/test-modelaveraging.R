1fe59e2b2116848ef5164fb9aaf24c993d1a2239
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2023-04-24 17:28:06 +0200

    formal test

--- tests/testthat/test-modelaveraging.R
+++ tests/testthat/test-modelaveraging.R
@@ -124,16 +124,19 @@ test_that("do_model_averaging() works", {
 
   # Even with non-numeric column #197
 
-  Achar <- data.frame(ID = rep(1:2, each = 2), type = rep(c("PRED", "IPRED"), 2), num = runif(4))
-  Bchar <- mutate(Achar, num = runif(4))
+  Achar <- data.frame(ID = rep(1:2, each = 2), type = rep(c("PRED", "IPRED"), 2), num = as.double(1:4))
+  Bchar <- mutate(Achar, num = as.double(5:8))
 
   expect_error(are_comparable(Achar, "foo")) # type of object
   expect_error(are_comparable(Achar, mutate(Bchar, foo = 1))) # number of variables
   expect_error(are_comparable(Achar, mutate(Bchar, ID = "foo"))) # type of variable
   expect_error(are_comparable(Achar, mutate(Bchar, type = "foo"))) # content of non-numeric variables
 
-  do_model_averaging(list_of_tabs = list(A = Achar, B = Bchar),
-                     weights_matrix = mat)
+  expect_equal(
+    do_model_averaging(list_of_tabs = list(A = Achar, B = Bchar),weights_matrix = mat),
+    data.frame(ID = rep(1:2, each = 2), type = rep(c("PRED", "IPRED")), num = c(2, 3, 3.4, 4.4))
+  )
+
 
 })
 
