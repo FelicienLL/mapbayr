83ed5fc2892dac7e0f7db5aa74077e389ac92e82
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2023-07-06 16:49:42 +0200

    document and tests

--- R/pcvpc.R
+++ R/pcvpc.R
@@ -1,48 +1,96 @@
-predcorr <- function(Yij, PREDbin, PREDij){
-  pcYij <- Yij * PREDbin / PREDij
-  pcYij[Yij==0 & PREDij==0] <- 0
-  pcYij
-}
-
-define_bin <- function(x, stratify_on, delta = delta){
-  x$roundidv <- floor(x$idv / delta) * delta
-  ans <- do.call(interaction, list(x[, c("roundidv", "name" , stratify_on)], sep = "_"))
-  return(as.character(ans))
-}
-
-prepare_vpc_sim <- function(data, delta = 1, end = NULL){
-  args <- tibble::tibble(data = split_mapbayr_data(data))
-  args <- mutate(args, ORIGID = names(data), .before = 1)
-  args$tgrid <- sapply(args$data, infer_tgrid, delta = delta, end = end)
-  args$obsaug <- TRUE
-  args$new_sigma <- "zero_re"
-
-  args
+#' Visual Predicted Checks
+#'
+#' @param x the model object
+#' @param data NMTRAN-like data set
+#' @param nrep a numeric, the number of replicates for stochastic simulations. Default is 500.
+#' @param pcvpc a logical, if `TRUE` (the default) will output "prediction-corrected VPC". Advised if several levels of doses or covariates are in the dataset for instance.
+#' @param idv a character indicating the variable used as independant variable. Default is "time", alternatively use "tad" to automatically compute the time after last dose.
+#' @param stratify_on a character (vector) indicating the variables of the data used to stratify the results. Variables must be numerical (as they are passed to [mrgsolve::carry_out()])
+#' @param start,end,delta,... passed to [mrgsolve::mrgsim()]
+#'
+#' @return a ggplot2 object, results of the VPC
+#' @export
+#'
+#' @examples
+#' library(mrgsolve)
+#' library(magrittr)
+#' # Define a model. Adding variability to house model because default is 0.
+#' mod <- house() %>%
+#'   omat(dmat(rep(0.2,4)))
+#'
+#' # Creating dataset for the example
+#' # Same concentration, but different dose (ID 2) and covariate (ID 3)
+#' data <- adm_rows(ID = 1, amt = 1000, cmt = 1, addl = 6, ii = 12) %>%
+#'   obs_rows(DV = 50, cmt = 2, time = 7 * 12) %>%
+#'   adm_rows(ID = 2, time = 0, amt = 2000, cmt = 1, addl = 6, ii = 12) %>%
+#'   obs_rows(DV = 50, cmt = 2, time = 7 * 12) %>%
+#'   adm_rows(ID = 3, time = 0, amt = 1000, cmt = 1, addl = 6, ii = 12) %>%
+#'   obs_rows(DV = 50, cmt = 2, time = 7 * 12) %>%
+#'   add_covariates(SEX = c(0,0,0,0,1,1))
+#'
+#' mapbayr_vpc(mod, data) # prediction-corrected by default
+#' mapbayr_vpc(mod, data, idv = "tad", start = 72)
+#' mapbayr_vpc(mod, data, idv = "tad", start = 72, pcvpc = FALSE)
+#' mapbayr_vpc(mod, data, idv = "tad", start = 72, stratify_on = "SEX")
+mapbayr_vpc <- function(x,
+                        data = NULL,
+                        nrep = 500,
+                        pcvpc = TRUE,
+                        idv = "time",
+                        stratify_on = NULL,
+                        start = NULL,
+                        end = NULL,
+                        delta = 1,
+                        ...){
+  vpc_plot(
+    vpc_sim(
+      x = x,
+      data = data,
+      nrep = nrep,
+      pcvpc = pcvpc,
+      idv = idv,
+      stratify_on = stratify_on,
+      start = start,
+      end = end,
+      delta = delta,
+      ... = ...
+    )
+  )
 }
 
 vpc_sim <- function(x,
                     data = NULL,
-                    nrep = 20,
+                    nrep = 500,
                     pcvpc = TRUE,
-                    delta = 1,
-                    idv = "tad",
-                    end = NULL,
+                    idv = "time",
                     stratify_on = NULL,
+                    start = NULL,
+                    end = NULL,
+                    delta = 1,
                     ...){
 
+  # Data
   if(is.null(data)){
     data <- x@args$data
   }
   x@args$data <- NULL #empty the data slot in model object
 
-  sim_args <- prepare_vpc_sim(data = data, delta = delta, end = end)
+  # Arguments used in iteration
+  sim_args <- prepare_vpc_sim(
+    data = data,
+    delta = delta,
+    end = end,
+    start = start
+    )
 
+  # Request
   if(any(c("PAR", "MET") %in% outvars(x)$capture)){
     Request <- c("PAR", "MET")
   } else {
     Request <- "DV"
   }
 
+  # Perform stochastic simulations
   stochastic_sim <- bind_rows(
     pmap(
       .l = sim_args,
@@ -58,24 +106,33 @@ vpc_sim <- function(x,
   ) %>%
     pivot_sims()
 
+  # Prepare OBSERVED data
+
   OBSTAB <- data
 
-  OBSTAB$idv <- filter(stochastic_sim,
-    .data$a.u.g == 0,
-    .data$ID == 1,
-    .data$name == unique(.data$name)[1]
-    )[[idv]]
+  ## Define independant variable (tad, time...)
 
+  OBSTAB$idv <- filter(stochastic_sim,
+                       .data$a.u.g == 0,
+                       .data$ID == 1,
+                       .data$name == unique(.data$name)[1]
+  )[[idv]]
+
+  ## Reframe (keep evid=0, name/value with PAR/MET)
   OBSTAB <- reframe_observations(OBSTAB)
 
+  ## Define bins
   OBSTAB$bin <- define_bin(OBSTAB, stratify_on = stratify_on, delta = delta)
 
-  stochastic_sim <- bind_rows(stochastic_sim) %>% as_tibble
+
+  # Prepare SIMULATED data
+
   SIMTAB <- stochastic_sim[stochastic_sim$a.u.g == 1, ]
   SIMTAB$idv <- SIMTAB[[idv]]
   SIMTAB$bin <- define_bin(SIMTAB, stratify_on = stratify_on, delta = delta)
 
   if(pcvpc){
+    # Simulated typical profiles
     typical_sim <- bind_rows(
       pmap(
         .l = sim_args,
@@ -91,16 +148,20 @@ vpc_sim <- function(x,
     ) %>%
       pivot_sims()
 
+    # Define bins
+
     typical_sim$idv <- typical_sim[[idv]]
     typical_sim[["bin"]][typical_sim$a.u.g==0] <- define_bin(typical_sim[typical_sim$a.u.g==0,], stratify_on = stratify_on, delta = delta)
     typical_sim[["bin"]][typical_sim$a.u.g==1] <- define_bin(typical_sim[typical_sim$a.u.g==1,], stratify_on = stratify_on, delta = delta)
 
-    # Median PRED tab
+    # Median PRED tab --> PREDbin variable
 
     medpredtab <- typical_sim %>%
       filter(.data$a.u.g == 1) %>%
       group_by(.data$bin) %>%
       summarise(PREDbin = stats::median(.data$value))
+
+    # Apply correction, on SIMTAB
 
     SIMTAB <- SIMTAB %>%
       left_join(medpredtab, by = "bin") %>%
@@ -111,6 +172,7 @@ vpc_sim <- function(x,
         PREDij = rep(typical_sim$value[typical_sim$a.u.g==1], nrep)
       )
       )
+    # Apply correction, on OBSTAB
 
     OBSTAB <- OBSTAB %>%
       left_join(medpredtab, by = "bin") %>%
@@ -173,4 +235,42 @@ vpc_plot <- function(vpc_sim, stratify_on = vpc_sim$stratify_on, idv = vpc_sim$i
   }
 
   p
+}
+
+predcorr <- function(Yij, PREDbin, PREDij){
+
+  # Yij = (simulated or observed) dependant variable
+  # PREDij = typical predicted depenant variable
+  # PREDbin = median of typical predictions in the given bin
+
+  # As described by Bergstrand et al, AAPS J 2011
+  # https://link.springer.com/article/10.1208/s12248-011-9255-z
+
+  pcYij <- Yij * PREDbin / PREDij
+  pcYij[Yij==0 & PREDij==0] <- 0
+  pcYij
+}
+
+define_bin <- function(x, stratify_on, delta = delta){
+  # x = a dataset
+  # stratify_on = character vector, variables to stratify on
+  # delta = delta used in simulations, useful here for the binning
+  x$roundidv <- floor(x$idv / delta) * delta
+  ans <- do.call(interaction, list(x[, c("roundidv", "name" , stratify_on)], sep = "_"))
+  return(as.character(ans))
+}
+
+prepare_vpc_sim <- function(data, start = NULL, end = NULL, delta = 1){
+  # Prepare the arguments that will be passed to pmap(.f = do_mapbayr_sim)
+
+  # data = full dataset
+  # start, end, delta = passed to mrgsim
+
+  args <- tibble::tibble(data = split_mapbayr_data(data))
+  args <- mutate(args, ORIGID = names(data), .before = 1)
+  args$tgrid <- sapply(args$data, infer_tgrid, delta = delta, end = end, start = start)
+  args$obsaug <- TRUE
+  args$new_sigma <- "zero_re"
+
+  args
 }
--- tests/testthat/test-pcvpc.R
+++ tests/testthat/test-pcvpc.R
@@ -1,6 +1,53 @@
 test_that("vpc_sim() works",{
-  exmodel(1, ID = 1:8) %>% vpc_sim() %>% vpc_plot()
-  exmodel(301, ID = 1:8) %>% vpc_sim() %>% vpc_plot()
-  exmodel(401, ID = 1:8) %>% vpc_sim() %>% vpc_plot()
+  expect_s3_class(exmodel(1,   ID = 1:2) %>% mapbayr_vpc(nrep = 10), "ggplot")
+  expect_s3_class(exmodel(301, ID = 1:2) %>% mapbayr_vpc(nrep = 10), "ggplot")
+  expect_s3_class(exmodel(401, ID = 1:2) %>% mapbayr_vpc(nrep = 10), "ggplot")
 })
 
+test_that("vpc_sim() works", {
+  library(mrgsolve)
+  mod <- house() %>%
+    omat(mrgsolve::dmat(rep(0.2,4)))
+
+  # Creating dataset for the example
+  # Same concentration, but different dose (ID 2) and covariate (ID 3)
+  data <- adm_rows(ID = 1, amt = 1000, cmt = 1, addl = 6, ii = 12) %>%
+    obs_rows(DV = 50, cmt = 2, time = 7 * 12) %>%
+    adm_rows(ID = 2, time = 0, amt = 2000, cmt = 1, addl = 6, ii = 12) %>%
+    obs_rows(DV = 50, cmt = 2, time = 7 * 12) %>%
+    adm_rows(ID = 3, time = 0, amt = 1000, cmt = 1, addl = 6, ii = 12) %>%
+    obs_rows(DV = 50, cmt = 2, time = 7 * 12) %>%
+    add_covariates(SEX = c(0,0,0,0,1,1))
+  vpcsim1 <- vpc_sim(mod, data, nrep = 10)
+  expect_type(vpcsim1, "list")
+  expect_named(vpcsim1, c("SIMTAB", "OBSTAB", "stratify_on", "idv"))
+
+  expect_s3_class(vpcsim1$SIMTAB, "data.frame")
+  expect_s3_class(vpcsim1$OBSTAB, "data.frame")
+  expect_null(vpcsim1$stratify_on)
+  expect_equal(vpcsim1$idv, "time")
+
+  # pcvpc works
+  expect_equal(vpcsim1$OBSTAB$value, c(73.84257, 36.92129, 50.00000), tolerance = 0.0001)
+
+  # idv = time, by default
+  expect_equal(vpcsim1$SIMTAB$time, vpcsim1$SIMTAB$idv)
+
+  vpcsim2 <- vpc_sim(mod, data, nrep = 20, idv = "tad", pcvpc = FALSE)
+  expect_equal(vpcsim2$OBSTAB$value, c(50, 50, 50))
+  expect_equal(nrow(vpcsim2$SIMTAB), nrow(vpcsim1$SIMTAB)*2)
+  expect_equal(unique(vpcsim2$SIMTAB$idv), seq(0,28,1))
+  expect_equal(vpcsim2$OBSTAB$value, c(50, 50, 50))
+
+  vpcsim3 <- vpc_sim(mod, data, nrep = 10, stratify_on = "SEX")
+  expect_equal(vpcsim3$stratify_on, "SEX")
+  expect_equal(vpcsim3$OBSTAB$value, c(75, 37.5, 50))
+  expect_length(intersect(
+    vpcsim3$SIMTAB$bin[vpcsim3$SIMTAB$SEX == 0],
+    vpcsim3$SIMTAB$bin[vpcsim3$SIMTAB$SEX == 1]
+  ), 0)
+
+  vpcsim4 <- vpc_sim(mod, data, nrep = 10, start = 72, end = 96, delta = 0.1)
+  expect_equal(unique(vpcsim4$SIMTAB$time), seq(72, 96, 0.1))
+
+})
